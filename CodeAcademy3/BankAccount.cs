436e27e69d17aa3a231d0d9e24103d9eff452bc2
Egis <155294980+nolessas@users.noreply.github.com>
2024-08-01 20:32:58 +0300

    Delete CodeAcademy3 directory

--- CodeAcademy3/BankAccount.cs
+++
@@ -1,8 +0,0 @@
-﻿public class BankAccount
-{
-    public Guid Id { get; set; }
-    public string CardNumber { get; set; }
-    public string Pin { get; set; }
-    public decimal Balance { get; set; }
-    public List<Transaction> Transactions { get; set; } = new List<Transaction>();
-}
--- CodeAcademy3/Transaction.cs
+++
@@ -1,7 +0,0 @@
-﻿public class Transaction
-{
-    public Guid Id { get; set; }
-    public DateTime Date { get; set; }
-    public decimal Amount { get; set; }
-    public string Type { get; set; }
-}
